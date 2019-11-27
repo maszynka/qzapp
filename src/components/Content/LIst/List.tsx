3f3edad9fb4b166b7db573b497a587b3ea2b54a8
Daniel Bogusz <daniel.bogusz@codestiny.pl>
2019-11-27 12:19:29 +0100

    fixed ts types for array of elements

--- src/components/Content/LIst/List.tsx
+++ src/components/Content/LIst/List.tsx
@@ -11,16 +11,42 @@ interface match {
 
 interface ListProps {
     match: match,
-    entries: [],
+    entries: EntryType[],
     type: string
 }
+
+interface QuestionType {
+    question: string,
+    answers: string[],
+    correct: number,
+    explanation: string
+}
+
+interface QuizType {
+    label: string,
+    alias: string,
+    questions: QuestionType[]
+}
+
+interface EntryType {
+    label: string,
+    alias: string,
+    groups: EntryType[],
+    quizzes: QuizType[]
+}
+
 // export const BreadcrumbsItem: React.FC<RouteComponentProps> = ({ match }) => {
 
 export const List: React.FC<ListProps> = ({ match, entries, type }) => {
     console.log(entries);
-    return entries.map((entry: object, i: number) => (
-        <Link to={slashSuffix(match.url) + entry.alias} key={i}>
-            {entry.label}
-        </Link>
-    ))
+    return (
+    <>
+        {
+            entries.map((entry, i) => (
+                <Link to={slashSuffix(match.url) + entry.alias} key={i}>
+                    {entry.label}
+                </Link>
+            )
+        }
+    </>)
 };
